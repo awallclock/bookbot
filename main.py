8786e4e9bc2229d7e3d993f0051ecce08167c5f1
Tim Witkovsky <tim.witkov@gmail.com>
2024-08-21 21:49:13 -0400

    added the final reporting

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = get_character_count(text)
-    # print(f"{character_count}")
-    # print(f"{word_count} words found in document")
-    separate_dictionary(character_count)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in document")
+    dict_list = separate_dictionary(character_count)
+    for i in range(0, len(dict_list)):
+        print(
+            f"The {dict_list[i]['character']} character was found {dict_list[i]['value']} times"
+        )
+    print("--- End report ---")
 
 
 def get_book_text(file):
@@ -22,18 +27,25 @@ def get_character_count(book):
     char_dict = {}
     for char in book:
         char = char.lower()
-        char_dict[char] = char_dict.setdefault(char, 0) + 1
+        if char.isalpha():
+            char_dict[char] = char_dict.setdefault(char, 0) + 1
     return char_dict
 
 
 def separate_dictionary(dictionary):
-    new_dictionary = {}
+    dictionary_list = []
     for character in dictionary:
         value = dictionary[character]
-        new_dictionary["character"] = character
-        new_dictionary["value"] = value
-    print(new_dictionary)
-    return
+        temp_dictionary = {}
+        temp_dictionary["character"] = character
+        temp_dictionary["value"] = value
+        dictionary_list.append(temp_dictionary)
+
+    def sort_on(dict):
+        return dict["value"]
+
+    dictionary_list.sort(reverse=True, key=sort_on)
+    return dictionary_list
 
 
 main()
